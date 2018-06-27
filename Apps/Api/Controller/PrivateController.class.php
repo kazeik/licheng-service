08a7cc5ad8796e6c7253b8fcc94d23bfc1fa4df0
kazeik@163.com <kazeik@163.com>
2018-06-27 15:31:55 +0800

    增加加油记录的接口

--- Apps/Api/Controller/PrivateController.class.php
+++ Apps/Api/Controller/PrivateController.class.php
@@ -3,9 +3,10 @@ namespace Api\Controller;
 use Think\Controller\RestController;
 
 abstract class PrivateController extends BaseController{
-
+    protected $uid;
     function __construct(){
         $uid = I('uid');
+        // $this->uid = $uid;
         if(empty($uid)){
             jsondata(null,"用户id不存在");
             die();
---
+++ Apps/Api/Controller/RecordController.class.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Api\Controller;
+use Think\Controller\RestController;
+
+class RecordController extends PrivateController{
+
+    function addRecord(){
+        $date = I("date");
+        $allLc = I("alllicheng");
+        $oilvalue = I("oilvalue");
+        $cMoney = I("currentmoney");
+        $aMoney = I("allmoney");
+        $oiltype = I("oiltype");
+        $about = I("about");
+        $uid = I('uid');
+        // $db = 
+
+        $data = Array(
+            'date'=>$date,      //时间
+            'uid'=>$uid,  // uid
+            'mileage'=>$allLc,   //总里程
+            'oiltype'=>$oiltype,   //油类型
+            'oilmoney'=>$oilvalue,  //油价
+            'allmoney'=>$aMoney,  //当次加油总价
+            'message'=>$about,   //备注
+            'oilmass'=>$oilvalue   //当前加油量
+        );
+        $flag = M('record')->data($data)->add();
+        jsondata($flag,$flag?"数据添加成功":"数据添加失败");
+    }
+}
